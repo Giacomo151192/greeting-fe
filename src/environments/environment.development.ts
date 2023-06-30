c0ff71cc2cb04e3c7435fddbae66211dd5af49f0
Giacomo Di Valerio <giacomo.divalerio@par-tec.it>
2023-06-30 10:51:03 +0200

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  //@ts-ignore
+  saluti: window["env"].SALUTI || "default"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  //@ts-ignore
+  saluti: window["env"]["apiUrl"] || "default"
+};
